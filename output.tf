c937e7093b2a30b179bd803fe5c452bb86d5e34d
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-08-17 01:52:43 +0530

    removed unwated variable

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "dns_records" {
-  description = "All created Cloudflare DNS records."
-  value = {
-    for k, rec in cloudflare_dns_record.this : k => {
-      id      = rec.id
-      name    = rec.name
-      type    = rec.type
-      content = rec.content
-      proxied = rec.proxied
-      ttl     = rec.ttl
-      hostname = rec.hostname
-    }
-  }
-}
+# output "dns_records" {
+#   description = "All created Cloudflare DNS records."
+#   value = {
+#     for k, rec in cloudflare_dns_record.this : k => {
+#       id      = rec.id
+#       name    = rec.name
+#       type    = rec.type
+#       content = rec.content
+#       proxied = rec.proxied
+#       ttl     = rec.ttl
+#       hostname = rec.hostname
+#     }
+#   }
+# }
